259d97ef68ce8ce545414f7d1e457a20e4b4b58f
Clément <clement.petrault@utbm.fr>
2019-05-12 14:03:50 +0200

    refactor

--- VueJeu.py
+++ VueJeu.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
 
+
 class VueJeu:
     """ Classe contenant l'interface graphique du jeu
     - model : Teeko -> contient les informations de la partie en cours
     - fond : contient l'image de fond du jeu
     - canvas : canvas contenant le jeu
     """
-
 
     def __init__(self, model, frame, actionOnMouseEvent):
         self.model = model
@@ -23,13 +23,13 @@ class VueJeu:
 
         for x in range(5):
             for y in range(5):
-                val = self.model.getCaseValue(x,y)
+                val = self.model.getCaseValue(x, y)
                 if val == 1:
-                    self.canvas.create_oval(65 + x * 82, 65 + y * 82, 105 + x * 82, 105 + y * 82, fill='red')
+                    self.canvas.create_oval(
+                        65 + x * 82, 65 + y * 82, 105 + x * 82, 105 + y * 82, fill='red')
                 if val == 2:
-                    self.canvas.create_oval(65 + x * 82, 65 + y * 82, 105 + x * 82, 105 + y * 82, fill='black')
+                    self.canvas.create_oval(
+                        65 + x * 82, 65 + y * 82, 105 + x * 82, 105 + y * 82, fill='black')
                 if val == 3:
-                    self.canvas.create_oval(75 + x * 82, 75 + y * 82, 95 + x * 82, 95 + y * 82, fill='blue')
-
-
-
+                    self.canvas.create_oval(
+                        75 + x * 82, 75 + y * 82, 95 + x * 82, 95 + y * 82, fill='blue')
