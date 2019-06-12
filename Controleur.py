c89ae47e4d3f88d921497964c2e2c32cb682621c
Clément <clement.petrault@utbm.fr>
2019-06-12 21:54:04 +0200

    feat: lancement du jeu avec parametre de niveaux de l'ia paramétrable

--- Controleur.py
+++ Controleur.py
@@ -30,22 +30,45 @@ class Controleur:
         self.root.mainloop()
 
     def creation_menu(self):
-        # TODO : modifié le menu (agencement + liste de niveau d'IA)
         self.frame.pack(fill=BOTH)
+
         self.choix_1 = IntVar()
         self.choix_2 = IntVar()
+
+        self.label_1 = Label(self.frame, text="Niveau IA 1 : ")
+        self.label_2 = Label(self.frame, text="Niveau IA 2 : ")
+
+        self.lb_1 = Listbox(self.frame, height=len(
+            parameters.tabLevels), selectmode=SINGLE, exportselection=0)
+        self.lb_2 = Listbox(self.frame, height=len(
+            parameters.tabLevels), selectmode=SINGLE, exportselection=0)
+
         choix_joueur1 = Radiobutton(
             self.frame, text="Joueur", variable=self.choix_1, value=0)
         choix_ia1 = Radiobutton(self.frame, text="IA",
                                 variable=self.choix_1, value=1)
         choix_joueur1.pack()
         choix_ia1.pack()
+
+        self.label_1.pack()
+
+        for i in range(1, len(parameters.tabLevels) + 1):
+            self.lb_1.insert(END, i)
+        self.lb_1.pack()
+
         choix_joueur2 = Radiobutton(
             self.frame, text="Joueur", variable=self.choix_2, value=0)
         choix_ia2 = Radiobutton(self.frame, text="IA",
                                 variable=self.choix_2, value=1)
         choix_joueur2.pack()
         choix_ia2.pack()
+
+        self.label_2.pack()
+
+        for i in range(1, len(parameters.tabLevels) + 1):
+            self.lb_2.insert(END, i)
+        self.lb_2.pack()
+
         bouton_jouer = Button(self.frame, text="Jouer",
                               command=self.lancement_jeu)
         bouton_jouer.pack()
@@ -73,9 +96,12 @@ class Controleur:
                             else:
                                 functarget = MinMax.min_max
 
+                            pMax = self.model.pMax_ias[self.model.tour]
+                            eval_enable = self.model.eval_ias[self.model.tour]
+
                             # Lance calcul de l'ia dans un thread
                             t = threading.Thread(
-                                target=functarget, args=(self.model, parameters.pMax))
+                                target=functarget, args=(self.model, pMax, eval_enable))
                             t.start()
 
                             # Attendre que l'IA joue...
@@ -101,9 +127,12 @@ class Controleur:
             else:
                 functarget = MinMax.min_max
 
+            pMax = self.model.pMax_ias[self.model.tour]
+            eval_enable = self.model.eval_ias[self.model.tour]
+
             # Lance calcul de l'ia dans un thread
             t = threading.Thread(
-                target=functarget, args=(self.model, parameters.pMax))
+                target=functarget, args=(self.model, pMax, eval_enable))
             t.start()
 
             # Attendre que l'IA joue...
@@ -116,7 +145,7 @@ class Controleur:
         self.fenetre_gagnant.title('Fin partie')
         self.fenetre_gagnant.geometry("200x100")
         champ_label = Label(self.fenetre_gagnant, text="Le joueur n°" +
-                                                       str(self.model.get_tour()) + " a gagné !")
+                            str(self.model.get_tour()) + " a gagné !")
         champ_label.pack()
         self.fenetre_gagnant.mainloop()
 
@@ -126,21 +155,61 @@ class Controleur:
         self.creation_menu()
 
     def lancement_jeu(self):
-        for widget in self.frame.winfo_children():
-            widget.destroy()
-        self.model = Model(self.choix_1.get(), self.choix_2.get())
-        # Si combat IA vs IA, alors lancer le jeu dans une console
-        if self.model.joueur1 == self.model.joueur2 == self.model.TYPE_IA:
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            # os.path.join(dir_path, 'VueConsole.py')
-            os.system('start /B start cmd.exe @cmd /k python ' +
-                      dir_path + "\VueConsole.py")
-            # self.iAvsIA()
-        # Sinon, lancer le GUI du jeu
-        else:
-            self.vue_jeu = VueJeu(self.model, self.frame,
-                                  self.action_on_mouse_event)
-            self.vue_jeu.update_status_label()
+        selection_level_ia_1 = self.lb_1.curselection()
+        selection_level_ia_2 = self.lb_2.curselection()
+
+        # Tester si un des joueurs est une IA
+        # et si un niveau est bien sélectionné
+        ready = True
+        if self.choix_1.get() == 1 and not selection_level_ia_1:
+            ready = False
+        if self.choix_2.get() == 1 and not selection_level_ia_2:
+            ready = False
+
+        # Si il y a une IA et que son niveau et sélectionné
+        if ready:
+            # Création du model
+            self.model = Model(self.choix_1.get(), self.choix_2.get())
+
+            # Récupération du niveau de chaque IA (si un des joueur est de type IA au moins)
+            # set pMax et eval boolean
+            if self.choix_1.get() == 1:
+                self.model.pMax_ias[1] = parameters.tabLevels[self.lb_1.get(
+                    ACTIVE) - 1][1]
+                self.model.eval_ias[1] = parameters.tabLevels[self.lb_1.get(
+                    ACTIVE) - 1][2]
+            if self.choix_2.get() == 1:
+                self.model.pMax_ias[2] = parameters.tabLevels[self.lb_2.get(
+                    ACTIVE) - 1][1]
+                self.model.eval_ias[2] = parameters.tabLevels[self.lb_2.get(
+                    ACTIVE) - 1][2]
+
+            # Si combat IA vs IA, alors lancer le jeu dans une console
+            if self.model.joueur1 == self.model.joueur2 == self.model.TYPE_IA:
+                dir_path = os.path.dirname(os.path.realpath(__file__))
+                # os.path.join(dir_path, 'VueConsole.py')
+                pMax_1 = str(
+                    parameters.tabLevels[self.lb_1.get(ACTIVE) - 1][1])
+                eval_1 = str(
+                    parameters.tabLevels[self.lb_1.get(ACTIVE) - 1][2])
+                pMax_2 = str(
+                    parameters.tabLevels[self.lb_2.get(ACTIVE) - 1][1])
+                eval_2 = str(
+                    parameters.tabLevels[self.lb_2.get(ACTIVE) - 1][2])
+
+                os.system('start /B start cmd.exe @cmd /k python ' +
+                          dir_path + "\VueConsole.py" + " " + pMax_1 + " " + eval_1 + " " + pMax_2 + " " + eval_2)
+                # Enleve le menu affiché
+                for widget in self.frame.winfo_children():
+                    widget.destroy()
+            # Sinon, lancer le GUI du jeu
+            else:
+                # Enleve le menu affiché
+                for widget in self.frame.winfo_children():
+                    widget.destroy()
+                self.vue_jeu = VueJeu(self.model, self.frame,
+                                      self.action_on_mouse_event)
+                self.vue_jeu.update_status_label()
 
     def creation_barre_menu(self):
         menubar = Menu(self.root)
